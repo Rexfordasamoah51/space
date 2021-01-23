a795766d76fb7ee6ab611c3f3e12666cef8b9cad
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2021-01-23 01:33:42 +0000

    start a new revolution space

---
+++ example/space_simple/android/app/src/main/kotlin/com/example/space_simple/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.space_simple
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/space.dart
@@ -0,0 +1,7 @@
+library space;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
